a1dee1a1c8b00d1d0165b7214fde3f80d9cce7b1
Allan Giovanni Matias <alangiovanepaes@gmail.com>
2025-09-22 20:58:38 -0300

    learning basics for and if

--- first-app/src/app/components/home/home.ts
+++ first-app/src/app/components/home/home.ts
@@ -1,22 +1,29 @@
+import { CommonModule } from '@angular/common';
 import { Component } from '@angular/core';
 import { RouterOutlet } from '@angular/router';
 
 @Component({
   selector: 'app-home',
-  imports: [],
+  imports: [CommonModule],
   templateUrl: './home.html',
   styleUrl: './home.css'
 })
 export class HomeComponent {
-  myBool: boolean = false;
+  mustShowTitle: boolean = false;
   name: string = "ana";
-  ariaLabel: string = "arial"
+  ariaLabel: string = "arial";
+  listItems = [
+    { id: 1, txt: "cu1" },
+    { id: 2, txt: "cu2" },
+    { id: 3, txt: "cu3" }
+  ];
 
   setMyBool(value: boolean) {
-    this.myBool = value;
+    this.mustShowTitle = value;
   }
 
   send() {
-    console.log('hello! you pressed the button.')
+    console.log('hello! you pressed the button.');
+    this.setMyBool(!this.mustShowTitle);
   }
 }
